ce4f8ec7ac2de881baf7c43894c26bdb4f3c3406
Kamaropoulos <kamaropoulos@outlook.com>
2018-07-28 15:24:22 +0300

    feat: App now starts from the new screen

--- src/navigation/root-navigator.ts
+++ src/navigation/root-navigator.ts
@@ -1,8 +1,10 @@
 import { createStackNavigator } from "react-navigation"
+import { TodoList } from "../views/TodoList/TodoList-screen"
 import { ExampleNavigator } from "./example-navigator"
 
 export const RootNavigator = createStackNavigator(
   {
+    todoList: { screen: TodoList },
     exampleStack: { screen: ExampleNavigator },
   },
   {
